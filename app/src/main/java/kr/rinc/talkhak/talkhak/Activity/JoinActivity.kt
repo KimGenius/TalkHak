8f56d86dc3a8bb681fc07012d058fbde8ed68e18
KimGenius <geniusk1047@naver.com>
2017-12-16 17:40:54 +0900

    join 추가

--- app/src/main/java/kr/rinc/talkhak/talkhak/Activity/JoinActivity.kt
+++ app/src/main/java/kr/rinc/talkhak/talkhak/Activity/JoinActivity.kt
@@ -1,5 +1,14 @@
 package kr.rinc.talkhak.talkhak.Activity
 
+import android.os.Bundle
+import kr.rinc.talkhak.talkhak.R
+
 /**
  * Created by geniusk on 2017. 12. 16..
  */
+class JoinActivity : BaseActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_join)
+  }
+}
